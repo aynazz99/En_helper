98e582e3f9cd142604763f6bbf0ab4d9e6a9b1a8
aynazz99 <101971937+aynazz99@users.noreply.github.com>
2025-09-03 01:55:46 +0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,6 @@
 const CACHE_NAME = 'wordquiz-cache-v' + new Date().getTime();
+const FILES_TO_CACHE = ['/', '/index.html', '/manifest.json', '/sw.js'];
 
-const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/sw.js'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES_TO_CACHE)).then(()=>self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null)))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(resp=>resp||fetch(event.request)));
-});
+self.addEventListener('install', e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate', e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null)))); self.clients.claim();});
+self.addEventListener('fetch', e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
