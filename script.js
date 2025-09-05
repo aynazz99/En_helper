161984bdcf1c6445d18eae933af761b9bded374a
aynazz99 <101971937+aynazz99@users.noreply.github.com>
2025-09-05 19:35:22 +0500

    Update script.js

--- script.js
+++ script.js
@@ -447,10 +447,11 @@ otherButtons.forEach(btn => {
 
 const listSelect = document.getElementById('listSelect');
 
-// При фокусе на селекте прячем форму
-listSelect.addEventListener('focus', () => {
+listSelect.addEventListener('change', () => {
+  // форма скрывается только после выбора нового списка
   addListForm.style.display = 'none';
 });
+
 
 
 // Отключение масштабирования
