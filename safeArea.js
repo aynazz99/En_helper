409de6773ea38ac7e9a5d05287a010fe44645684
aynazz99 <101971937+aynazz99@users.noreply.github.com>
2025-09-21 20:36:25 +0500

    Add files via upload

--- safeArea.js
+++ safeArea.js
@@ -1,18 +1,16 @@
-const BASE_PADDING = 35; // тот самый padding, который был раньше
-
 document.addEventListener("DOMContentLoaded", () => {
     const tg = window.Telegram?.WebApp;
     if (!tg) return;
+
     tg.ready();
 
+    const safeTop = tg.viewportStableOffsetTop || 0;
     const header = document.querySelector('header');
+    if (header) header.style.transform = `translateY(${safeTop}px)`;
 
-    function applyTelegramSafeArea() {
+    // Чтобы реагировать на изменение размера экрана
+    window.addEventListener("resize", () => {
         const safeTop = tg.viewportStableOffsetTop || 0;
-        if (header) header.style.transform = `translateY(${safeTop + BASE_PADDING}px)`;
-    }
-
-    applyTelegramSafeArea();
-    window.addEventListener("resize", applyTelegramSafeArea);
+        if (header) header.style.transform = `translateY(${safeTop}px)`;
+    });
 });
-
