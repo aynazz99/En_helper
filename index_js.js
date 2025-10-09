7deab294d54624b0047dc74f7f1a33f2c9441759
aynazz99 <101971937+aynazz99@users.noreply.github.com>
2025-10-09 14:19:26 +0500

    Update index_js.js

--- index_js.js
+++ index_js.js
@@ -7,4 +7,5 @@ setInterval(() => {
   cards.forEach(card => card.style.transform = 'scale(1)');
   cards[current].style.transform = 'scale(1.1)';
   current = (current + 1) % cards.length;
-}, 800);
+}, 1000);
+
